1a5bd2c553b97231222c0eb1258f8d2820eeee5f
akiyoshi-takazwa <takazawa@arsaga.jp>
2021-07-14 22:05:39 +0900

    create admin route

---
+++ config/domain.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'admin_host'        => env('APP_DOMAIN_PREFIX') . 'admin.' . env('APP_DOMAIN', 'cart-memo.test'),
+    'user_host'        => env('APP_DOMAIN_PREFIX') . 'www.' . env('APP_DOMAIN', 'cart-memo.test'),
+];
