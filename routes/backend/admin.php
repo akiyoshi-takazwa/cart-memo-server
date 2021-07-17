9edb35d2874b0a69f045c98be6ef7ce1a88e3341
akiyoshi-takazwa <takazawa@arsaga.jp>
2021-07-17 17:37:04 +0900

    create home

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Backend\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Backend\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Backend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthenticatedSessionController::class, 'create'])
@@ -10,6 +12,11 @@ Route::get('/', [AuthenticatedSessionController::class, 'create'])
 Route::post('/', [AuthenticatedSessionController::class, 'store'])
     ->middleware('guest');
 
-Route::get('/password-rest', [AuthenticatedSessionController::class, 'create'])
+Route::get('/password-rest', [PasswordResetLinkController::class, 'create'])
     ->middleware('guest')
     ->name('password-reset');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', HomeController::class)
+        ->name('home');
+});
