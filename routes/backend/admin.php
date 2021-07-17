0cd0b51a801b3942b3901206c75f67ac713a5ac5
akiyoshi-takazwa <takazawa@arsaga.jp>
2021-07-17 16:38:10 +0900

    create admin-login

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Backend\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('backend.auth.login');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest');
