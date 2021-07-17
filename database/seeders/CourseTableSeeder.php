b9238e06e3391a52ba06a025d89a273bad71a5b6
akiyoshi-takazwa <takazawa@arsaga.jp>
2021-07-17 17:57:13 +0900

    create model

---
+++ database/seeders/CourseTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\UserProfile;
+use Database\Factories\UserProfileFactory;
+use Illuminate\Database\Seeder;
+
+class CourseTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}
