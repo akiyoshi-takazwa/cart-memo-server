bb058b107313b0045630d6f2037e548560f9deaf
akiyoshi-takazwa <takazawa@arsaga.jp>
2021-09-12 23:54:09 +0900

    fix countBy key

--- app/Services/Frontend/Analysis/WinRateCalculate.php
+++ app/Services/Frontend/Analysis/WinRateCalculate.php
@@ -36,15 +36,15 @@ class WinRateCalculate
         $first = isset($countBy[1]) ? round($countBy[1]/$count * 100, 2) : '-';
         $second = isset($countBy[2]) ? round($countBy[2]/$count * 100, 2) : '-';
         $third = isset($countBy[3]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $fourth = isset($countBy[4]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $fifth = isset($countBy[5]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $sixth = isset($countBy[6]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $seventh = isset($countBy[7]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $eighth = isset($countBy[8]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $ninth = isset($countBy[9]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $tenth = isset($countBy[10]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $eleventh = isset($countBy[11]) ? round($countBy[3]/$count * 100, 2) : '-';
-        $twelfth = isset($countBy[12]) ? round($countBy[3]/$count * 100, 2) : '-';
+        $fourth = isset($countBy[4]) ? round($countBy[4]/$count * 100, 2) : '-';
+        $fifth = isset($countBy[5]) ? round($countBy[5]/$count * 100, 2) : '-';
+        $sixth = isset($countBy[6]) ? round($countBy[6]/$count * 100, 2) : '-';
+        $seventh = isset($countBy[7]) ? round($countBy[7]/$count * 100, 2) : '-';
+        $eighth = isset($countBy[8]) ? round($countBy[8]/$count * 100, 2) : '-';
+        $ninth = isset($countBy[9]) ? round($countBy[9]/$count * 100, 2) : '-';
+        $tenth = isset($countBy[10]) ? round($countBy[10]/$count * 100, 2) : '-';
+        $eleventh = isset($countBy[11]) ? round($countBy[11]/$count * 100, 2) : '-';
+        $twelfth = isset($countBy[12]) ? round($countBy[12]/$count * 100, 2) : '-';
 
         return [
             'average'  => $average,
